df75bc1863f92038c0f43e29d64a92ba90c441dd
Cyrus <cyrus.frost@hotmail.com>
2019-09-06 22:49:55 +0530

    init express app

---
+++ server.js
@@ -0,0 +1,30 @@
+
+// @ts-check
+
+import { spawnSync } from 'child_process';
+import express from 'express';
+import bodyParser from 'body-parser';
+import { inspect } from 'util';
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+const port = 3000;
+
+
+app.get('/', (req, res) => {
+  const ls = spawnSync('find', ['/sdcard/Pictures/']);
+  res.send(ls.stdout.toString());
+});
+
+app.post('/', (req, res) => {
+  console.log(req.body.shell_cmd);
+  const command = req.body.shell_cmd;
+  // command = command.shell_cmd;
+  console.log('command to exec is: ', command);
+  res.send('success');
+});
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
